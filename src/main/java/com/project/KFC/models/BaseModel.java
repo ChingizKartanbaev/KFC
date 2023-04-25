33aa759adcb19f1f47a0ff1046aab7b40986cae8
Operans <chingiz.kartanbaev@gmail.com>
2023-04-25 08:34:01 +0600

    update model

--- src/main/java/com/project/KFC/models/BaseModel.java
+++ src/main/java/com/project/KFC/models/BaseModel.java
@@ -1,4 +1,39 @@
 package com.project.KFC.models;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JoinColumn(name = "add_date")
+    LocalDateTime addDate;
+    @JoinColumn(name = "update_date")
+    LocalDateTime updateDate;
+    boolean active;
+
+
+    @PrePersist
+    protected void onCreate() {
+        addDate = LocalDateTime.now();
+        updateDate = LocalDateTime.now();
+        active=true;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updateDate = LocalDateTime.now();
+    }
+
 }
