cf5b68174fdb4e5507477c1dfa8e15de13345980
Operans <chingiz.kartanbaev@gmail.com>
2023-05-04 21:29:24 +0600

    update

--- src/main/java/com/project/KFC/controllers/ProductController.java
+++ src/main/java/com/project/KFC/controllers/ProductController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,6 +27,10 @@ public class ProductController {
         return "product/index";
     }
 
+    @PostMapping("/addToTheBasket/{id}")
+    public String addToTheBasket(@PathVariable("id") Long id) {
+        return "";
+    }
 
 }
 
