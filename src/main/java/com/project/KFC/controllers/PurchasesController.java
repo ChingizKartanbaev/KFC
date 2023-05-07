2674f1d12eafe1f64d8028e6c1895576be04d1ee
Operans <chingiz.kartanbaev@gmail.com>
2023-05-07 12:36:47 +0600

    now you can complete the order

--- src/main/java/com/project/KFC/controllers/PurchasesController.java
+++ src/main/java/com/project/KFC/controllers/PurchasesController.java
@@ -1,11 +1,16 @@
 package com.project.KFC.controllers;
 
+import com.project.KFC.enums.PurchasesStatusEnum;
+import com.project.KFC.models.Purchases;
 import com.project.KFC.services.CustomerService;
+import com.project.KFC.services.EmployeeService;
 import com.project.KFC.services.PurchasesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,11 +19,13 @@ public class PurchasesController {
 
     private final PurchasesService purchasesService;
     private final CustomerService customerService;
+    private final EmployeeService employeeService;
 
     @Autowired
-    public PurchasesController(PurchasesService purchasesService, CustomerService customerService) {
+    public PurchasesController(PurchasesService purchasesService, CustomerService customerService, EmployeeService employeeService) {
         this.purchasesService = purchasesService;
         this.customerService = customerService;
+        this.employeeService = employeeService;
     }
 
     @GetMapping("/buy")
@@ -41,5 +48,14 @@ public class PurchasesController {
     public String getProcessPurchases(Model model) {
         model.addAttribute("purchases", customerService.getProcessPurchasesFromCustomer());
         return "purchases/index";
+    }
+
+    @PutMapping("/complete/{id}")
+    public String completeOrder(@PathVariable("id") Long id) {
+        Purchases purchases = purchasesService.findById(id);
+        purchases.setEmployee(employeeService.getEmployee());
+        purchases.setPurchasesStatus(PurchasesStatusEnum.READY);
+        purchasesService.save(purchases);
+        return "redirect:/purchases/getProcessPurchases";
     }
 }
