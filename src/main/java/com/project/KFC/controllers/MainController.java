8fed6012d15be5af34b887ad52aebe636ad189f1
Operans <chingiz.kartanbaev@gmail.com>
2023-05-23 12:08:05 +0600

    update auth

--- src/main/java/com/project/KFC/controllers/MainController.java
+++ src/main/java/com/project/KFC/controllers/MainController.java
@@ -1,5 +1,7 @@
 package com.project.KFC.controllers;
 
+import com.project.KFC.services.CustomerService;
+import com.project.KFC.services.EmployeeService;
 import com.project.KFC.services.ProductsService;
 import com.project.KFC.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,27 +17,49 @@ public class MainController {
 
     private final UserService userService;
     private final ProductsService productsService;
+    private final CustomerService customerService;
+    private final EmployeeService employeeService;
 
     @Autowired
-    public MainController(UserService userService, ProductsService productsService) {
+    public MainController(UserService userService, ProductsService productsService, CustomerService customerService, EmployeeService employeeService) {
         this.userService = userService;
         this.productsService = productsService;
+        this.customerService = customerService;
+        this.employeeService = employeeService;
     }
 
     @GetMapping()
     public String index(Model model) {
-        model.addAttribute("menus", productsService.findAll());
+        model.addAttribute("user", customerService.getCustomer() == null ? employeeService.getEmployee() : customerService.getCustomer())
+                .addAttribute("customer", customerService.getCustomer())
+                    .addAttribute("menus", productsService.findAll())
+                .addAttribute("employee",employeeService.getEmployee());
         return "index";
     }
 
     @GetMapping("/about")
-    public String about() {return "about";}
+    public String about(Model model) {
+        model.addAttribute("user", customerService.getCustomer() == null ? employeeService.getEmployee() : customerService.getCustomer())
+                .addAttribute("customer", customerService.getCustomer())
+                .addAttribute("employee",employeeService.getEmployee());
+        return "about";
+    }
 
     @GetMapping("/contact")
-    public String contact() {return "contact";}
+    public String contact(Model model) {
+        model.addAttribute("user", customerService.getCustomer() == null ? employeeService.getEmployee() : customerService.getCustomer())
+                .addAttribute("customer", customerService.getCustomer())
+                .addAttribute("employee",employeeService.getEmployee());
+        return "contact";
+    }
 
     @GetMapping("/blog")
-    public String blog() {return "blog";}
+    public String blog(Model model) {
+        model.addAttribute("user", customerService.getCustomer() == null ? employeeService.getEmployee() : customerService.getCustomer())
+                .addAttribute("customer", customerService.getCustomer())
+                .addAttribute("employee",employeeService.getEmployee());
+        return "blog";
+    }
 
     @GetMapping("/error")
     public String errorPage() {
